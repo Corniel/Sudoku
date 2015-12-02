d9917ede2b7dc6eb3003a00ea8ae2826b1a266d3
Corniel <mail@corniel.nl>
2015-12-02 09:03:47 +0100

    Added puzzles and centralized testing logic.

--- src/Corniel.Sudoku.UnitTests/SudokuSolverTest.cs
+++ src/Corniel.Sudoku.UnitTests/SudokuSolverTest.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using NUnit.Framework.Compatibility;
+using System;
 
 namespace Corniel.Sudoku.UnitTests
 {
@@ -6,32 +8,42 @@ namespace Corniel.Sudoku.UnitTests
 	public class SudokuSolverTest
 	{
 		[Test]
+		public void Solve_2x2Invalid_ThrowsInvalidPuzzleException()
+		{
+			Assert.Catch<InvalidPuzzleException>(() =>
+			{
+				var puzzle = SudokuState.Parse(@"
+				.2|..
+				2.|..
+				--+--
+				..|..
+				..|..");
+				var solver = new SudokuSolver(SudokuPuzzle.Puzzle2x2);
+				solver.Solve(puzzle);
+			});
+
+		}
+		[Test]
 		public void Solve_2x2_SolvedPuzzle()
 		{
-			var puzzle = SudokuState.Parse(@"
+			Solve(SudokuPuzzle.Puzzle2x2, @"
 				.4|.1
 				3.|..
 				--+--
 				..|.4
-				..|..");
-
-			var solver = new SudokuSolver(SudokuPuzzle.Puzzle2x2);
-
-			var act = solver.Solve(puzzle);
-			var exp = SudokuState.Parse(@"
+				..|..",
+				@"
 				24|31
 				31|42
 				--+--
 				13|24
 				42|13");
-
-			Assert.AreEqual(exp, act);
 		}
 
 		[Test]
 		public void Solve_3x3WikipediaExample_SolvedPuzzle()
 		{
-			var puzzle = SudokuState.Parse(@"
+			Solve(SudokuPuzzle.Puzzle3x3, @"
 				53.|.7.|...
 				6..|195|...
 				.98|...|.6.
@@ -42,12 +54,8 @@ namespace Corniel.Sudoku.UnitTests
 				---+---|---
 				.6.|...|28.
 				...|419|..5
-				...|.8.|.79");
-
-			var solver = new SudokuSolver(SudokuPuzzle.Puzzle3x3);
-
-			var act = solver.Solve(puzzle);
-			var exp = SudokuState.Parse(@"
+				...|.8.|.79",
+				@"
 				534|678|912
 				672|195|348
 				198|342|567
@@ -59,14 +67,12 @@ namespace Corniel.Sudoku.UnitTests
 				961|537|284
 				287|419|635
 				345|286|179");
-
-			Assert.AreEqual(exp, act);
 		}
 
 		[Test]
-		public void Solve_3x3Level4_SolvedPuzzle()
+		public void Solve_3x3Example1_SolvedPuzzle()
 		{
-			var puzzle = SudokuState.Parse(@"
+			Solve(SudokuPuzzle.Puzzle3x3, @"
 				6..|..4|..3
 				..5|7.6|.1.
 				.1.|...|7..
@@ -77,12 +83,8 @@ namespace Corniel.Sudoku.UnitTests
 				---+---+---
 				4..|5..|..7
 				.6.|...|1..
-				3..|69.|..2");
-
-			var solver = new SudokuSolver(SudokuPuzzle.Puzzle3x3);
-
-			var act = solver.Solve(puzzle);
-			var exp = SudokuState.Parse(@"
+				3..|69.|..2",
+				@"
 				672|914|583
 				845|736|219
 				913|258|746
@@ -94,14 +96,71 @@ namespace Corniel.Sudoku.UnitTests
 				421|583|967
 				569|472|138
 				387|691|452");
+		}
 
-			Assert.AreEqual(exp, act);
+		[Test]
+		public void Solve_3x3Example2_SolvedPuzzle()
+		{
+			Solve(SudokuPuzzle.Puzzle3x3, @"
+				...|317|.69
+				16.|.92|..3
+				..9|...|...
+				---+---+---
+				41.|7..|..8
+				5.3|9.4|6.7
+				7..|..8|.32
+				---+---+---
+				...|...|3..
+				9..|83.|.46
+				63.|549|...",
+				@"
+				254|317|869
+				168|492|573
+				379|685|124
+				---+---+---
+				412|763|958
+				583|924|617
+				796|158|432
+				---+---+---
+				841|276|395
+				925|831|746
+				637|549|281");
+		}
+
+		[Test]
+		public void Solve_3x3Example3_SolvedPuzzle()
+		{
+			Solve(SudokuPuzzle.Puzzle3x3, @"
+				...|7..|...
+				1..|...|...
+				...|43.|2..
+				---+---+---
+				...|...|..6
+				...|5.9|...
+				...|...|418
+				---+---+---
+				...|.81|...
+				..2|...|.5.
+				.4.|...|3..
+				",
+				@"
+				264|715|839
+				137|892|645
+				598|436|271
+				---+---+---
+				423|178|596
+				816|549|723
+				759|623|418
+				---+---+---
+				375|281|964
+				982|364|157
+				641|957|382");
 		}
 
 		[Test]
 		public void Solve_3x3WorldsHardestStudokuAccordingToTheTelegraph_SolvedPuzzle()
 		{
-			var puzzle = SudokuState.Parse(@"
+			Solve(SudokuPuzzle.Puzzle3x3, @"
 				8..|...|...
 				..3|6..|...
 				.7.|.9.|2..
@@ -113,12 +172,8 @@ namespace Corniel.Sudoku.UnitTests
 				..1|...|.68
 				..8|5..|.1.
 				.9.|...|4..
-				");
-
-			var solver = new SudokuSolver(SudokuPuzzle.Puzzle3x3);
-
-			var act = solver.Solve(puzzle);
-			var exp = SudokuState.Parse(@"
+				",
+				@"
 				8..|...|...
 				..3|6..|...
 				.7.|.9.|2..
@@ -130,8 +185,21 @@ namespace Corniel.Sudoku.UnitTests
 				..1|...|.68
 				..8|5..|.1.
 				.9.|...|4..");
+		}
 
-			Assert.AreEqual(exp, act);
+		private void Solve(SudokuPuzzle puzzle, string input, string expected)
+		{
+			var state = SudokuState.Parse(input);
+			var solver = new SudokuSolver(puzzle);
+
+			var sw = Stopwatch.StartNew();
+			var actual = solver.Solve(state);
+			sw.Stop();
+
+			Console.WriteLine("Elapsed: {0:#,##0.#####} ms", sw.Elapsed.TotalMilliseconds);
+
+			Assert.IsTrue(actual.IsSolved(), "The puzzle is not solved.");
+			Assert.AreEqual(SudokuState.Parse(expected), actual);
 		}
 	}
 }

197ba924914238cd6134ebe54776265daebc25ef
Corniel Nobel <mail@corniel.nl>
2022-03-02 08:40:58 +0100

    Add link

--- src/SudokuSolver/Technique.cs
+++ src/SudokuSolver/Technique.cs
@@ -1,6 +1,7 @@
 ﻿namespace SudokuSolver;
 
 /// <summary>A technique to (partly) solve a Sudoku puzzle.</summary>
+/// <remarks>See: http://www.taupierbw.be/SudokuCoach/SC_index.shtml</remarks>
 public interface Technique
 {
     /// <summary>Reduces the options per cell.</summary>

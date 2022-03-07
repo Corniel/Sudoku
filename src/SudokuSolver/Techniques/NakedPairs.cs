12be4d7d75f85fdfd3dd287e8bd63b7eb2a1f994
Corniel Nobel <mail@corniel.nl>
2022-03-07 16:38:31 +0100

    Allow multiple naked pairs to be found.

--- src/SudokuSolver/Techniques/NakedPairs.cs
+++ src/SudokuSolver/Techniques/NakedPairs.cs
@@ -8,7 +8,37 @@
 /// All other appearances of the two candidates in the same row, column,
 /// or block can be eliminated.
 /// </remarks>
-public class NakedPairs : NakedMultiple
+public class NakedPairs : Technique
 {
-    protected override int Size => 2;
+    private const int Size = 2;
+
+    /// <inheritdoc />
+    public Puzzle Reduce(Puzzle puzzle, Regions regions)
+    {
+        foreach (var cell in puzzle.Where(c => c.Values.Count == Size))
+        {
+            foreach (var region in regions[cell.Location])
+            {
+                puzzle = CheckCells(cell.Values, region, puzzle);
+            }
+        }
+        return puzzle;
+    }
+
+    private static Puzzle CheckCells(Values naked, Region region, Puzzle puzzle)
+    {
+        var count = 0;
+        foreach(var cell in puzzle.Region(region))
+        {
+            if (cell.Values == naked && ++count > Size) return puzzle;
+        }
+        if (count == Size)
+        {
+            foreach (var cell in puzzle.Region(region).Where(c => c.Values != naked))
+            {
+                puzzle = puzzle.Not(cell.Location, naked);
+            }
+        }
+        return puzzle;
+    }
 }

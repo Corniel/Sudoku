3d28b759ff04464cd4dd435c4e51a89b22f56fde
Corniel Nobel <corniel@gmail.com>
2025-09-19 15:41:53 +0200

    Introduction of Anti-King

--- SudokuSolver/Rules.cs
+++ SudokuSolver/Rules.cs
@@ -8,6 +8,9 @@ public static class Rules
     /// <summary>The standard set of houses.</summary>
     public static readonly ImmutableArray<Constraint> Standard = [.. Row.All, .. Col.All, .. Box.All];
 
+    /// <summary>The standard set of housed extended with the <see cref="Constraints.AntiKing"/> restrictions.</summary>
+    public static readonly ImmutableArray<Constraint> AntiKing = [.. Standard, .. Constraints.AntiKing.All];
+
     /// <summary>The standard set of housed extended with the <see cref="Constraints.AntiKnight"/> restrictions.</summary>
     public static readonly ImmutableArray<Constraint> AntiKnight = [.. Standard, .. Constraints.AntiKnight.All];
 

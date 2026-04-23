af1ff78381391c3a334015aa51ebbcbf46f3d31d
Corniel Nobel <corniel@gmail.com>
2026-04-23 21:20:44 +0200

    Solved 2026-04-20: Williwaw

---
+++ Puzzles/CrackingTheCryptic/2026/2026-04-20.cs
@@ -0,0 +1,44 @@
+namespace Puzzles.CrackingTheCryptic;
+
+public sealed class _2026_04_20 : CtcPuzzle
+{
+    public override string Title => "Williwaw";
+
+    public override string? Author => "Nicolas Duhail";
+
+    public override Uri? Url => new("https://youtu.be/0cvA-XDiQNQ");
+
+    public override O Duration => O.ms100;
+
+    public override Cells Solution { get; } = Cells.Parse("""
+        584│312│976
+        629│758│431
+        137│964│258
+        ───┼───┼───
+        715│436│892
+        863│291│547
+        492│875│163
+        ───┼───┼───
+        276│149│385
+        958│623│714
+        341│587│629
+        """);
+
+    protected override Rules GetConstraints()
+        => Rules.Standard
+        + SameSums.Parse("""
+        ...│...│...
+        ACY│XX.│xca
+        ACY│..X│xca
+        ───┼───┼───
+        B.D│ZZy│db.
+        ...│.Z.│...
+        EE.│Ggg│.ee
+        ───┼───┼───
+        F.F│H.h│f.f
+        .F.│.Hh│.fi
+        III│H..│ii.
+        A=B C=D E=F G=H=I X=Y=Z
+        a=b c=d e=f g=h=i x=y
+        """);
+}
--- Sudoku/Common/SameSums.cs
+++ Sudoku/Common/SameSums.cs
@@ -8,13 +8,18 @@ public static partial class SameSums
     {
         var matches = Pattern().Matches(str);
 
-        var end = matches.Count is 0 ? str.Length : matches.OfType<Match>().Select(m => m.Index).Min();
+        var end = matches.Count is 0 ? str.Length : matches.OfType<Match>().Min(m => m.Index);
 
         var groups = Groups.Parse(str[..end]);
+
+        HashSet<char> unique = [.. groups.Select(g => g.Key)];
 
         foreach (Match match in matches)
         {
             var names = match.ToString().Where(char.IsAsciiLetter).ToArray();
+
+            foreach (var n in names)
+                unique.Remove(n);
 
             if (names.Any(n => !groups.ContainsKey(n)))
                 throw new FormatException($"The '{string.Concat(names)}' group is not fully coverted");
@@ -22,6 +27,9 @@ public static partial class SameSums
             foreach (var res in SameSum.Create([..names.Select(n => groups[n].ToImmutableArray())]))
                 yield return res;
         }
+
+        if (unique.Any())
+            throw new FormatException($"The '{string.Concat(unique)}' groups are not described");
 
         if (matches.Count is 0)
             foreach (var res in SameSum.Create([..groups.Select(g => g.Value.ToImmutableArray())]))
